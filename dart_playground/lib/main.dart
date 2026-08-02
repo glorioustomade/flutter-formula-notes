7ed72eb3af403a10b6bb755c1e101a72c98ed751
Orimadegun Oluwatomi Glory <tomade@Orimadeguns-MacBook-Pro.local>
2026-08-02 13:48:50 +0100

    lesson 02 Assignment 2 submission

---
+++ dart_playground/lib/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  var name = "Tomade";
+  var age = 27;
+  var height = 1.75;
+  bool isLearningFlutter = true;
+
+print("My name is $name ");
+print("I am $age years old");
+print("My height is $height");
+print("Am I learning Flutter? $isLearningFlutter");
+
+
+
+var favoriteFood = "Rice";
+var favoriteColor = "Purple";
+var yearsLearningFlutter = 1;
+
+print("My favorite food is $favoriteFood");
+print("My favorite color is $favoriteColor");
+print("I have been learning Flutter for $yearsLearningFlutter year");
+}
+
