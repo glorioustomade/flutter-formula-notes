b14cdd200ac50d109384106ecdb9c5072bdf4bab
Orimadegun Oluwatomi Glory <tomade@Orimadeguns-MacBook-Pro.local>
2026-08-03 22:03:26 +0100

    submission mini challenge lesson 3

---
+++ dart_playground/challenge.dart
@@ -0,0 +1,18 @@
+void main(){
+  var name = "Orimadegun Oluwatomi Glory";
+  int age = 27;
+  var color = "blue";
+  List<String> favouriteFood = ["Rice", "Beans"];
+  Set programmingLanguage = {"Dart", "Javascript"};
+  Map profile = {
+    "name": "Orimadegun Oluwatomi Glory",
+    "age": 27,
+    "country": "Nigeria",
+    "profession": "Software Engineer",
+   
+  };
+  print("My name is $name");
+  print("My fav food is $favouriteFood[1] ");
+  print("My profession is $profile['profession']");
+
+}
