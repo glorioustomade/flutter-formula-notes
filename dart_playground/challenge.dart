62508ea947c2382bc4e0d7adde9a587c66cb81ae
Orimadegun Oluwatomi Glory <tomade@Orimadeguns-MacBook-Pro.local>
2026-08-03 22:05:13 +0100

    ignore minor error

--- dart_playground/challenge.dart
+++ dart_playground/challenge.dart
@@ -1,8 +1,11 @@
 void main(){
   var name = "Orimadegun Oluwatomi Glory";
+  // ignore: unused_local_variable
   int age = 27;
+  // ignore: unused_local_variable
   var color = "blue";
   List<String> favouriteFood = ["Rice", "Beans"];
+  // ignore: unused_local_variable
   Set programmingLanguage = {"Dart", "Javascript"};
   Map profile = {
     "name": "Orimadegun Oluwatomi Glory",
